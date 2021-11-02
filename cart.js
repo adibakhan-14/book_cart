a3e222398fa7dad095efb82f907376e1b951c838
Adiba_14 <adibasumaiya.27@gmail.com>
2021-11-02 13:49:44 +0600

    first commit

---
+++ cart.js
@@ -0,0 +1,19 @@
+//first book
+const bookOne = document.querySelector('#cart-item1');
+const alertOne = document.querySelector('#alert-message1');
+const closeOne = document.querySelector('#close1');
+const undoOne = document.querySelector('#aalert-link1');
+const qntBookOne = document.querySelector('#quantity-box-1');
+const decrementBookOne = document.querySelector('#decrement1');
+const rmvBookOne = document.querySelector('#rvm-btn2');
+const priceBookOne = document.querySelector('#price2'); //secone book
+const bookTwo = document.querySelector('#cart-item2');
+const alertTwo = document.querySelector('#alert-message2');
+const closeTwo = document.querySelector('#close2');
+const undoTwo = document.querySelector('#aalert-link2');
+const qntBookTwo = document.querySelector('#quantity-box-2');
+const decrementBookTwo = document.querySelector('#decrement2');
+const rmvBookTwo = document.querySelector('#rvm-btn2');
+const priceBookTwo = document.querySelector('#price2');
+
+//rest of the elements
