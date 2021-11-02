4f3e65ce6ab7465ffb3a3d179bae34ba09916328
Adiba_14 <adibasumaiya.27@gmail.com>
2021-11-02 13:58:58 +0600

    changes

--- cart.js
+++ cart.js
@@ -17,3 +17,22 @@ const rmvBookTwo = document.querySelector('#rvm-btn2');
 const priceBookTwo = document.querySelector('#price2');
 
 //rest of the elements
+const cartTotal = document.querySelector('#cart-total');
+const subTotal = document.querySelector('#subTotal');
+const total = document.querySelector('#total');
+const payable = document.querySelector('#payable');
+
+//functionality for book one
+rmvBookOne.addEventListener('click', function() {
+    bookOne.style.display = 'none';
+    alertOne.style.display = 'block';
+    alertTwo.style.display = 'none';
+
+});
+undoOne.addEventListener('click', function() {
+    bookOne.style.display = 'block';
+    alertOne.style.display = 'none'
+});
+closeOne.addEventListener('click', function() {
+    alertOne.style.display = 'none';
+})
